c531b86a6f747f442c1129f2cf37735a54e4d088
patriciaaguayo <patriciaaguayop@gmail.com>
2025-01-21 20:49:21 +0100

    Completando estructura básica del proyecto.

--- proyectoHibernate/src/main/java/org/example/entities/Familia.java
+++ proyectoHibernate/src/main/java/org/example/entities/Familia.java
@@ -1,4 +1,71 @@
 package org.example.entities;
 
-public class Familia {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "familias")
+public class Familia implements Serializable {
+
+    @Id
+    @Column(name = "Dni")
+    private String dni;
+
+    @Column(name = "Edad", nullable = false)
+    private Integer edadPersona;
+
+    @Column(name = "Nombre", nullable = false)
+    private String nombrePersona;
+
+    @Column(name = "Ciudad")
+    private String ciudad;
+
+    // CONSTRUCTORES
+
+    public Familia() {
+    }
+
+    public Familia(String dni, Integer edad, String nombre, String ciudad) {
+        this.dni = dni;
+        this.edadPersona = edad;
+        this.nombrePersona = nombre;
+        this.ciudad = ciudad;
+    }
+
+    // GETTERS
+
+    public String getDni() { return dni; }
+
+    public Integer getEdad() { return edadPersona; }
+
+    public String getNombre(){ return nombrePersona; }
+
+    public String getCiudad() { return ciudad; }
+
+    // SETTERS
+
+    public void setDni(String dni) { this.dni = dni; }
+
+    public void setEdad(Integer edad) { this.edadPersona = edad; }
+
+    public void setNombre(String nombre) { this.nombrePersona = nombre; }
+
+    public void setCiudad(String ciudad) { this.ciudad = ciudad; }
+
+    // TO STRING
+
+    @Override
+    public String toString() {
+        return "Familia{" +
+                "dni='" + dni + '\'' +
+                ", edad=" + edadPersona +
+                ", nombre='" + nombrePersona + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                '}';
+    }
+
 }
--- proyectoHibernate/src/main/java/org/example/entities/Refugio.java
+++ proyectoHibernate/src/main/java/org/example/entities/Refugio.java
@@ -1,4 +1,58 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "refugio")
 public class Refugio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "Nombre_Refugio", nullable = false)
+    private String nombreRefugio;
+
+    @ManyToOne
+    @JoinColumn(name = "Dni", nullable = false)
+    private Familia familia;
+
+    // CONSTRUCTORES
+
+    public Refugio() {}
+
+    public Refugio(Familia familia) {
+        this.familia = familia;
+        this.nombreRefugio = "Prado Verde";
+    }
+
+    public Refugio(Familia familia, String nombreRefugio) {
+        this.familia = familia;
+        this.nombreRefugio = nombreRefugio;
+    }
+
+    // GETTERS
+
+    public Familia getFamilia() { return familia; }
+
+    public Integer getId() { return id; }
+
+    public String getNombreRefugio() { return nombreRefugio; }
+
+    // SETTERS
+
+    public void setFamilia(Familia familia) { this.familia = familia; }
+
+    public void setNombreRefugio(String nombreRefugio) { this.nombreRefugio = nombreRefugio; }
+
+    // TO STRING
+
+    @Override
+    public String toString() {
+        return "Refugio{" +
+                "id=" + id +
+                ", familia=" + familia +
+                ", nombreRefugio='" + nombreRefugio + '\'' +
+                '}';
+    }
 }
